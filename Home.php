121dbc3f65570cae6bb7bc5827ddac58556bbce3
Maria Eduarda <maria.silva1@aluno.ifsp.edu.br>
2021-05-07 17:23:15 -0300

    Criação da Home e css

---
+++ Home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="Aparencia.css">
+</head>
+<body>
+    <header>
+        <input type="checkbox" id="btn-menu">
+        <label for="btn-menu">&#9776;</label>
+        <nav class="menu">
+            <ul>
+                <li><a href="">Cadastro</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </nav>
+    </header>
