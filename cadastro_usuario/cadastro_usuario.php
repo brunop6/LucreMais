78b9e57d50f72476324842a7c502f3becc77cd72
brunop6 <peresbruno6@gmail.com>
2021-05-06 22:10:51 -0300

    Back-End cadastro de usuários

---
+++ cadastro_usuario/cadastro_usuario.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Usuário</title>
+</head>
+<body>
+    <?php
+        include '../includes/conecta_bd.inc';
+        include '../includes/encrypt.inc';
+
+        $usuario = $_POST['usuario'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        $senha = PasswordEncrypt($usuario, $email, $senha);
+
+        $query = "INSERT INTO usuario VALUES ('$usuario', '$email', '$senha')";
+        
+        $result = mysqli_query($conexao, $query);
+
+        if($result){
+            echo '<h2>Cadastro realizado com sucesso!</h2>';
+            echo "<p><a href='../login/login.html'><button>Retornar ao login</button></a></p>";
+        }else{
+            echo '<h2>Erro ao realizar cadastro...</h2> <br>';
+            echo mysqli_error($conexao);
+            echo "<p><a href='../login/login.html'><button>Retornar ao login</button></a></p>";  
+        }
+        mysqli_close($conexao);
+    ?>
+</body>
+</html>
