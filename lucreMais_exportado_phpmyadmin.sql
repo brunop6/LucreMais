07c7963f50d8b02ab7ad2813af568bfbbacae9e5
Kssiany <83619541+Kssiany@users.noreply.github.com>
2021-05-11 18:23:01 -0300

    SCRIPT BANCO DE DADOS
    Foram feitas alterações no script e adicionadas mais 2 tabelas no banco, uma delas a tabela fornecedor e a outra categoria.
    Foi adicionado também o idUsuario em todas as outras tabelas e um comando para pegar data e hora padrão do cadastro e atualização da mesma.

--- lucreMais_exportado_phpmyadmin.sql
+++ lucreMais_exportado_phpmyadmin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08-Maio-2021 às 20:34
+-- Tempo de geração: 11-Maio-2021 às 23:13
 -- Versão do servidor: 10.4.18-MariaDB
 -- versão do PHP: 8.0.5
 
@@ -24,18 +24,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idUsuario` int(10) UNSIGNED NOT NULL,
+  `descricaoCategoria` varchar(250) NOT NULL,
+  `dataCadastro` datetime NOT NULL DEFAULT current_timestamp(),
+  `dataAtualizacao` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fornecedor`
+--
+
+CREATE TABLE `fornecedor` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idUsuario` int(10) UNSIGNED NOT NULL,
+  `nomeFornecedor` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `cnpj` varchar(50) DEFAULT NULL,
+  `endereco` varchar(150) DEFAULT NULL,
+  `dataCadastro` datetime NOT NULL DEFAULT current_timestamp(),
+  `dataAtualizacao` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `item`
 --
 
 CREATE TABLE `item` (
   `id` int(10) UNSIGNED NOT NULL,
   `idUsuario` int(10) UNSIGNED NOT NULL,
+  `idFornecedor` int(10) UNSIGNED NOT NULL,
+  `idCategoria` int(10) UNSIGNED NOT NULL,
   `nome` varchar(50) NOT NULL,
   `quantidade` int(11) NOT NULL,
   `unidadeMedida` varchar(10) NOT NULL,
   `preco` double NOT NULL,
   `quantidadeMinima` int(11) NOT NULL,
-  `lote` int(11) NOT NULL
+  `lote` int(11) NOT NULL,
+  `dataCadastro` datetime NOT NULL DEFAULT current_timestamp(),
+  `dataAtualizacao` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -48,7 +84,9 @@ CREATE TABLE `usuario` (
   `id` int(10) UNSIGNED NOT NULL,
   `nomeUsuario` varchar(50) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `senha` varchar(50) NOT NULL
+  `senha` varchar(50) NOT NULL,
+  `dataCadastro` datetime NOT NULL DEFAULT current_timestamp(),
+  `dataAtualizacao` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -56,11 +94,29 @@ CREATE TABLE `usuario` (
 --
 
 --
+-- Índices para tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_ibfk_1` (`idUsuario`);
+
+--
+-- Índices para tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_nomeFornecedor` (`nomeFornecedor`),
+  ADD UNIQUE KEY `unique_cnpj` (`cnpj`),
+  ADD KEY `fornecedor_ibfk_1` (`idUsuario`);
+
+--
 -- Índices para tabela `item`
 --
 ALTER TABLE `item`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `item_ibfk_1` (`idUsuario`);
+  ADD KEY `item_ibfk_1` (`idUsuario`),
+  ADD KEY `item_ibfk_2` (`idFornecedor`),
+  ADD KEY `item_ibfk_3` (`idCategoria`);
 
 --
 -- Índices para tabela `usuario`
@@ -72,6 +128,18 @@ ALTER TABLE `usuario`
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `item`
@@ -90,10 +158,24 @@ ALTER TABLE `usuario`
 --
 
 --
+-- Limitadores para a tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD CONSTRAINT `categoria_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`) ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  ADD CONSTRAINT `fornecedor_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`) ON UPDATE CASCADE;
+
+--
 -- Limitadores para a tabela `item`
 --
 ALTER TABLE `item`
-  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `item_ibfk_2` FOREIGN KEY (`idFornecedor`) REFERENCES `fornecedor` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `item_ibfk_3` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
