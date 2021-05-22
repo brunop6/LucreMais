6c060979e37f411289ff4b97ab717642d2bfaac5
Bruno Peres <peresbruno6@gmail.com>
2021-05-22 00:50:13 -0300

    Controle n° ingredientes p/ Receita

--- Receitas/cadastrar_receitas.php
+++ Receitas/cadastrar_receitas.php
@@ -9,20 +9,23 @@
     <h1>Cadastro de Receitas<br></h1>
 
     <form action="" method="POST" id="formulario">
-    <p><input type="submit" value="Cadastrar Receita" id="cadastrar"></p><br>
-    <p><input type="text" name="nomeReceita" placeholder="Nome da receita"></p>
-    <p><input type="text" name="ingrediente" placeholder="Ingrediente"></p>
-    <p><input type="number" name="quantidade" placeholder="Quantidade"></p>
-    <select id="unidade_de_medida" >
-        <option value="unidade_de_medida">Unidade de Medida</option> 
-        <option value="ml">ML</option>
-        <option value="grama">Grama</option>
-        <option value="colher_de_sopa">Colher de Sopa</option>
-        <option value="colher_de_cha">Colher de Chá</option>
-        <option value="colher_de_cafe">Colher de Café</option>
-        <option value="xicara">Xícara</opition>
-    </select>
-    <p><button id="inserir" onclick="inseriringrediente()">Inserir ingrediente</button></p>
+        <!--Parágrafo invisível para controle do número de ingredientes-->
+        <p id="numIngred" hidden>1</p>
+
+        <p><input type="submit" value="Cadastrar Receita" id="cadastrar"></p><br>
+        <p><input type="text" name="nomeReceita" placeholder="Nome da receita"></p>
+        <p><input type="text" name="ingrediente1" placeholder="1° Ingrediente"></p>
+        <p><input type="number" name="quantidade" placeholder="Quantidade"></p>
+        <select id="unidade_de_medida" >
+            <option value="unidade_de_medida">Unidade de Medida</option> 
+            <option value="ml">ML</option>
+            <option value="grama">Grama</option>
+            <option value="colher_de_sopa">Colher de Sopa</option>
+            <option value="colher_de_cha">Colher de Chá</option>
+            <option value="colher_de_cafe">Colher de Café</option>
+            <option value="xicara">Xícara</opition>
+        </select>
+        <p><button id="inserir" onclick="inseriringrediente()">Inserir ingrediente</button></p>
     </form>
 </body>
 </html>
--- Receitas/criacampo.js
+++ Receitas/criacampo.js
@@ -1,34 +1,73 @@
 function inseriringrediente(){
-    document.getElementById("inserir").remove();
+    document.getElementById("inserir").remove();  
+    
+    //Controle da quantidade de ingredientes
+    var pNum = document.getElementById("numIngred")
+    var numIngred = parseInt(pNum.textContent);
+    numIngred++;
 
+    //Novos elementos
     var p1 = document.createElement("p");
     var p2 = document.createElement("p");
+    var p3 = document.createElement("p");
     var inputi = document.createElement("input");
-    var inserir = document.createElement("button");
     var inputq = document.createElement("input");
+    var inserir = document.createElement("button");   
     var select = document.createElement("select");
-    var option = document.createElement("option");
+    var option1 = document.createElement("option");
+    var option2 = document.createElement("option");
+    var option3 = document.createElement("option");
+    var option4 = document.createElement("option");
+    var option5 = document.createElement("option");
+    var option6 = document.createElement("option");
+    var option7 = document.createElement("option");
 
-    inputi.placeholder = "Ingrediente";
+    inputi.type = "text";
+    inputi.name = "ingrediente"+numIngred;
+    inputi.placeholder = numIngred+"° Ingrediente";
+    
+    inputq.type = "number";
+    inputq.placeholder = "Quantidade";
+
+    option1.textContent = "Unidade de Medida";
+    option1.value = "unidade_de_medida";
+    option2.textContent = "ML";
+    option2.value = "ml";
+    option3.textContent = "Grama";
+    option3.value = "grama";
+    option4.textContent = "Colher de Sopa";
+    option4.value = "colher_de_sopa";
+    option5.textContent = "Colher de Chá"
+    option5.value = "colher_de_cha";
+    option6.textContent = "Colher de Café";
+    option6.value = "colher_de_café";
+    option7.textContent = "Xícara";
+    option7.value = "xicara";
+
     inserir.textContent = "Inserir ingrediente";
     inserir.id = "inserir";
     inserir.addEventListener("click", inseriringrediente);
 
-    inputq.type = "number";
-    inputq.placeholder = "Quantidade";
-
-    option
-
     var form = document.getElementById("formulario");
 
     form.appendChild(p1);
-    p1.appendChild(inputi);
     form.appendChild(p2);
-    p2.appendChild(inputq);
     form.appendChild(select);
-    select.appendChild(option);
+    form.appendChild(p3);
 
-    form.appendChild(inserir);
+    p1.appendChild(inputi);
+    p2.appendChild(inputq);
+    p3.appendChild(inserir);
+
+    select.appendChild(option1);
+    select.appendChild(option2);
+    select.appendChild(option3);
+    select.appendChild(option4);
+    select.appendChild(option5);
+    select.appendChild(option6);
+    select.appendChild(option7);
 
     document.body.appendChild(form);
+
+    pNum.textContent = numIngred;
 }
