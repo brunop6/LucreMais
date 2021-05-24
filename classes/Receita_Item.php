84ad03a4640ffd99fa816c54db6f007dc4dc049c
Rafael-Francisco21 <83609474+Rafael-Francisco21@users.noreply.github.com>
2021-05-24 19:49:37 -0300

    Classe Receita_Item

--- classes/Receita_Item.php
+++ classes/Receita_Item.php
@@ -1,42 +1,46 @@
 <?php
-    class Receita{
+    class Receita_Item{
         private $idReceita;
         private $idItem;
         private $quantidade;
+        private $unidadeMedida;
         
 
-        function __construct($idReceita, $idItem, $quantidade)
+        function __construct($idReceita, $idItem, $quantidade, $unidadeMedida)
         {
             $this->idReceita = $idReceita;
             $this->idItem = $idItem;
             $this->quantidade = $quantidade;
+            $this->unidadeMedida = $unidadeMedida;
         }
 
-        public static function selectReceita_Itens(){
+        public static function selectReceita_Itens($idReceita){
             include '../includes/conecta_bd.inc';
 
-            $query = "SELECT idItem, quantidade FROM receita_item WHERE idReceita = '$idReceita' ";
+            $query = "SELECT idItem, quantidade, unidadeMedida FROM receita_item WHERE idReceita = '$idReceita' ";
 
             $resultado = mysqli_query($conexao, $query);
             $idItem = null;
             $quantidade = null;
+            $unidadeMedida = null;
             if(mysqli_num_rows($resultado) > 0){
                 $i = 0;
                 while($row = mysqli_fetch_array($resultado)){
                     $idItem[$i] = $row['idItem'];
                     $quantidade[$i] = $row['quantidade'];
+                    $unidadeMedida[$i] = $row['unidadeMedida'];
                     $i++;
                 }
             }
             mysqli_close($conexao);
 
-            return $nomeReceita;
+            return array ($idItem, $quantidade, $unidadeMedida);
         }
 
         public function cadastrarReceita_Item(){
             include '../includes/conecta_bd.inc';
 
-            $query = "INSERT INTO receita_item (idReceita, idItem, quantidade) VALUES ('$this->idReceita', '$this->idItem', '$this->quantidade')";
+            $query = "INSERT INTO receita_item (idReceita, idItem, quantidade, unidadeMedida) VALUES ('$this->idReceita', '$this->idItem', '$this->quantidade', $this->unidadeMedida)";
 
             $resultado = mysqli_query($conexao, $query);
 
