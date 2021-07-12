b238d2b0f8d209ea0aeacf651de314fb0d877688
Kssiany <83619541+Kssiany@users.noreply.github.com>
2021-07-12 12:58:35 -0300

    SCRIPT BANCO DE DADOS ATUALIZADO
    Metas 20/07/21 - Banco de dados

--- lucreMais_exportado_phpmyadmin.sql
+++ lucreMais_exportado_phpmyadmin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12-Jul-2021 às 17:38
+-- Tempo de geração: 12-Jul-2021 às 17:55
 -- Versão do servidor: 10.4.18-MariaDB
 -- versão do PHP: 8.0.5
 
