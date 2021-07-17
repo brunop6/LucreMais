0418778e1b3c65518e317720695a96450af75b58
Cintia02 <83596348+Cintia02@users.noreply.github.com>
2021-07-17 15:40:33 -0300

    Add files via upload

---
+++ usuario/edita_usuario.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="edita_usuario.css">
+    <title>Edição de Usuário</title>
+</head>
+<body>
+    <img src="../Logo.png" alt="Logo do Site" width="14%">
+    <form action="" method="POST">
+        <h3>Edição de Usuário</h3>
+        
+        <p><input type="text" name="nome" placeholder="Nome"></p>
+           
+        <p><input type="text" name="senha" placeholder="Senha"></p>
+
+        <p><input type="submit" value="Salvar"></p>
+        
+        <p><input type="button" value="Voltar" onclick="window.location.href='./../Home.php'"></p>
+</form>
+</body>
+</html>
