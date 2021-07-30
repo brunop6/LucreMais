db99592cd30fec5ce659dad87730bd3ad117ac13
Cintia02 <83596348+Cintia02@users.noreply.github.com>
2021-07-30 13:06:11 -0300

    Add files via upload

---
+++ cadastro_funcionario/cadastro_funcionario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro funcionário</title>
+    <link rel="stylesheet" href="aparenciafuncionario.css">
+</head>
+<body>
+<img src="./../Logo.png" alt="Logo do site" width="14%">
+    <h1> Cadastro de Funcionário </h1>
+       <form action="cadastrar_funcionario.php" method="POST">
+            <p><input type="text" name="usuario" placeholder="Usuário" id="em" required></p>
+            <p><input type="password" name="senha" placeholder="Senha" id="s" onchange="confirmar_senha()" required></p>
+            <p><input type="password" placeholder="Confirme sua senha" id="sc" onkeyup="confirmar_senha()" required></p>
+            <select name="nivel_de_acesso" id="niveldeacesso" required>
+                <option value="nivel_de_acesso"> Nível de Acesso </option>
+                <option value="administrador"> Administrador </option>
+                <option value="estoquista"> Estoquista </option>
+            </select>
+            <p><input type="submit" value="Cadastrar-se"> 
+            <p><input type="button" value="Voltar" onclick="window.location.href='./../Home.php'"></p>
+       </form>
+       
+</body>
+</html>
