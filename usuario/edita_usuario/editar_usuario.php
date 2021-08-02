fa1199de610964b0d2f402354458e23b261e805f
Rafael-Francisco21 <83609474+Rafael-Francisco21@users.noreply.github.com>
2021-08-02 19:12:32 -0300

    Add files via upload

---
+++ usuario/edita_usuario/editar_usuario.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Login/aparencial.css">
+    <title>Editar Usuário</title>
+</head>
+<body>
+    <?php
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+        include_once '../.././classes/Usuario.php';
+
+        $nomeUsuario = $_POST['nome'];
+        $senhaUsuario = $_POST['senha'];
+    
+        $usuario = new Usuario('0', $nomeUsuario, $_SESSION["email_usuario"], $senhaUsuario);
+    
+        print ($usuario->selectId($nomeUsuario));
+    
+        $idUsuario = $usuario->selectId($nomeUsuario);
+
+        $resultado = $usuario->editarContaUsusario($idUsuario);
+
+        if($resultado){
+            echo "<h2>Senha alterada com sucesso!</h2> <br>";
+              $_SESSION["senha_usuario"] = $senhaUsuario;
+        }else{
+            echo '<h2>Erro ao alterar senha...</h2> <br>';
+            echo "<p lang='en'>".$resultado."</p>";  
+        }
+        echo "<p><a href='../../Home.php'><button>Retornar a página principal</button></a></p>";
+    print ($resultado)
+    ?>
+</body>
+</html>
